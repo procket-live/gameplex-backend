d01a01d112dd3327d514007e3644e8c4d4361c67
Himanshu Kushwah <himanshu.kushwaha@drivezy.com>
2019-11-25 13:28:27 +0530

    dsf

--- api/controllers/dashboard.controller.js
+++ api/controllers/dashboard.controller.js
@@ -5,14 +5,12 @@ const Platform = require('../models/platform.model');
 
 exports.get_dashboard_meta = async (req, res) => {
     try {
-        const games = await Game.find().exec();
-        const platforms = await Platform.find().exec();
+        const games = await Game.find().populate('game_meta.lookup_type').exec();
 
         res.status(201).json({
             success: true,
             response: {
                 game: games,
-                platform: platforms,
                 draft: 0,
                 active: 0,
                 completed: 0
