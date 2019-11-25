fc1081c23905ed44df3d198958528889550abbbc
Himanshu Kushwah <himanshu.kushwaha@drivezy.com>
2019-11-25 13:19:44 +0530

    dsf

--- api/routes/dashboard.route.js
+++ api/routes/dashboard.route.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const DashboardController = require('../controllers/dashboard.controller');
 const checkAuth = require('../middlewares/check-auth');
 
-router.get('/', checkAuth, DashboardController.get_dashboard_meta);
+router.get('/', DashboardController.get_dashboard_meta);
 
 module.exports = router;
