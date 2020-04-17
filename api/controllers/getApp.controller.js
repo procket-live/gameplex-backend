9fd0068e7b387401eca7a7b53245be125997dc3d
Himanshu Kushwah <himanshu.kushwaha@drivezy.com>
2020-04-17 15:29:44 +0530

    link changed

--- api/controllers/getApp.controller.js
+++ api/controllers/getApp.controller.js
@@ -2,8 +2,7 @@ const sendSMS = require('../../utils/send-sms');
 
 exports.get_app = async (req, res) => {
     const mobile = req.query['mobile'];
-    console.log('mobile', mobile);
-    const appLink = 'https://firebasestorage.googleapis.com/v0/b/gameplex-5a29e.appspot.com/o/apk%2Fgameplex-2-4-21-1e632g94r.apk?alt=media&token=d5d8a434-4020-419e-a320-e227e147044a';
+    const appLink = 'https://www.gameplex.app';
 
     sendSMS(mobile, '<#> Welcome to gameplex, \n Here is your link to download the App. OTP 0000\n\n ' + appLink);
 
