f2cd7e288238d5f6dc7b5388b40ef08b40e60f55
Himanshu Kushwah <himanshu.kushwaha@drivezy.com>
2020-04-26 11:53:36 +0530

    yoyo

--- api/job/tournament-job.js
+++ api/job/tournament-job.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Tournament = require('../models/tournament.model');
 const RemoveUnUsedJob = require('./remove-unused-match');
 const Battle = require('../models/battle.model');
+const Participent = require('../models/participent.model');
 
 const GAMEPLEX_USER_ID = '5e8b820063a73d0017b149da';
 const GAME_ID = '5e9e08ce7060761dc9d78c18';
@@ -63,11 +64,11 @@ exports.create_all_flappy_bird_tournament = async () => {
         }
     });
 
-    create_tournament_flappy_bird("Hopping Freeway", 0, 12);
-    create_tournament_flappy_bird("Hopping Silver", 5, 12);
-    create_tournament_flappy_bird("Hopping Gold", 10, 12);
-    create_tournament_flappy_bird("Hopping Platinum", 25, 12);
-    create_tournament_flappy_bird("Hopping Diamond", 50, 12);
+    create_tournament_flappy_bird("Hopping Freeway", 0, 10);
+    create_tournament_flappy_bird("Hopping Silver", 5, 10);
+    create_tournament_flappy_bird("Hopping Gold", 10, 10);
+    create_tournament_flappy_bird("Hopping Platinum", 25, 10);
+    create_tournament_flappy_bird("Hopping Diamond", 50, 10);
 }
 
 exports.complete_tournament = async () => {
@@ -101,6 +102,10 @@ exports.complete_tournament = async () => {
             .exec();
         const tournamentObjectParticipents = tournamentObject.participents || [];
 
+        if (tournament.payout_released) {
+            return;
+        }
+
         if (tournamentObjectParticipents.length < 2) {
             for (let j = 0; j < tournamentObjectParticipents.length; j++) {
                 const participent = tournamentObjectParticipents[j];
@@ -115,6 +120,68 @@ exports.complete_tournament = async () => {
                     payout_released: true
                 }
             })
+        } else {
+            // relese payout and complete tournament
+            const rankWiseParticipent = tournamentObjectParticipents.sort((a = {}, b = {}) => {
+                // a should come before b in the sorted order
+                if (a.result_meta.score < b.result_meta.score) {
+                    return 1;
+                    // a should come after b in the sorted order
+                } else if (a.result_meta.score > b.result_meta.score) {
+                    return -1;
+                    // a and b are the same
+                } else {
+                    return 0;
+                }
+            });
+
+            NotifyAmountMatchComplete(rankWiseParticipent, tournament);
+
+            //set ranking
+            for (let i = 0; i < rankWiseParticipent.length; i++) {
+                const item = rankWiseParticipent[i];
+
+                await Participent.findByIdAndUpdate(item._id, { $set: { 'rank_meta.rank': i + 1 } });
+            }
+
+            //release payout
+            const rankWiseAmount = {};
+
+            tournament.rank.forEach((rankItem) => {
+                rankWiseAmount[rankItem.rank] = rankItem.amount;
+            })
+
+
+            rankWiseParticipent.forEach(async (item = {}) => {
+                const resultMeta = item.result_meta || {};
+                const rank = resultMeta.rank;
+                const userId = item.user._id;
+                let winAmount = 0;
+
+                if (rank) {
+                    winAmount += rankWiseAmount[rank] || 0;
+                }
+
+                if (winAmount > 0) {
+                    const walletTransaction = {
+                        amount: winAmount,
+                        target: "win_balance",
+                        source: tournamentId,
+                        source_name: "Tournament"
+                    };
+
+                    await User.findByIdAndUpdate(userId, {
+                        $inc: {
+                            wallet_win_balance: Number(Math.abs(winAmount))
+                        },
+                        $push: {
+                            wallet_transactions: walletTransaction
+                        }
+                    }).exec();
+                }
+            })
+
+            await Tournament.findByIdAndUpdate(tournamentId, { $set: { payout_released: true, status: "completed", ranking_set: true } })
         }
     })
 
@@ -128,5 +195,16 @@ async function NotifyAboutAmountRefund(participents = [], tournament = {}) {
     Notify.notify(tokens, {
         title: "Amount Refuned",
         body: tournamentName + " was cancelled. your amount is refuned to your wallet."
+    })
+}
+
+async function NotifyAmountMatchComplete(participents = [], tournament = {}) {
+    const tournamentName = tournament.tournament_name;
+    const tokens = participents.map((participent) => participent.user.firebase_token);
+
+    Notify.notify(tokens, {
+        title: "Tournament Completed",
+        body: tournamentName + " is complted. check your tournament rank now!!!",
+        data: { route: "Tournament", value: tournament._id }
     })
 }
